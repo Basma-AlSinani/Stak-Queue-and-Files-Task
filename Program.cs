3e66773fe7371ffd1e43f2492ee36eabecd6e487
Basma Suliman <basmaalsinani97@gmail.com>
2025-04-21 15:39:12 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Stak__Queue_and_Files_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Reverse a String Using Stack
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();//to store charecter
+
+            foreach(char c in input)//to enter the charecter into stack by using stack push
+            {
+                stack.Push(c);
+            }
+            string reversed = "";
+            while (stack.Count > 0)
+            {
+                reversed =reversed+ stack.Pop();
+            }
+            Console.WriteLine("Reversed string: " + reversed);
+        }
+    }
+}
